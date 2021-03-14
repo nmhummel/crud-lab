e857f9e844724f032c74c5129f890c3c2737433f
nmhummel <natalie.hummel@gmail.com>
2021-03-13 22:47:58 -0600

    commit tree f258e232b5c5b8bf6005ee2c8ace3f98f9f0a2ea

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
+//  allows a user to create new restaurants. Your form should have two inputs: a text input to enter the name of the restaurant, and an input with type equal submit.
 class RestaurantInput extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addRestaurant(this.state.text);
+    this.setState({
+      text: '',
+    });
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={ e => this.handleSubmit(e) }>
+          <input type="text" value={this.state.name} onChange={ e => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
